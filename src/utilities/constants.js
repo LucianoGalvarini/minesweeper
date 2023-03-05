bd997e12b0d840013df76191b75f3945037d8895
LucianoGalvarini <galvariniluciano@gmail.com>
2023-03-05 13:46:18 -0300

    fixed

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,5 @@
 export const MINESWEEPER_LEVELS = {
-  easy: { name: "easy", rows: 3, cols: 3, mines: 1 },
+  easy: { name: "easy", rows: 9, cols: 9, mines: 10 },
   medium: { name: "medium", rows: 16, cols: 16, mines: 40 },
   hard: { name: "hard", rows: 16, cols: 30, mines: 99 },
 };
